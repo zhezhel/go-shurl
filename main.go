0da1b23c404bbc9d01609ef5a40138ce9901b669
Andrii Zhezhel <andrii.zhezhel@gmail.com>
2017-10-04 13:07:08 +0200

    Add basic routs

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-type String string
+	"github.com/gorilla/mux"
+)
 
 func reverse(value string) string {
 	data := []rune(value)
@@ -14,7 +17,7 @@ func reverse(value string) string {
 	return string(result)
 }
 
-func dec_to_62(symbol string, n int) string {
+func dec_to_base62(symbol string, n int) string {
 	var url string
 	for int(n/62) > 0 {
 		url += string(symbol[n%62])
@@ -24,7 +27,33 @@ func dec_to_62(symbol string, n int) string {
 	return reverse(url)
 }
 
-func main() {
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://google.com", http.StatusMovedPermanently)
+	fmt.Println(mux.Vars(r)["id"])
+}
+func create(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println(dec_to_62("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 3843))
+	fmt.Println("create")
+}
+func info(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("info")
+}
+func view(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("view")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", view).Methods("GET")
+	r.HandleFunc("/", create).Methods("POST")
+	r.HandleFunc("/{id:[a-zA-Z0-9]+}", info).Methods("POST")
+	r.HandleFunc("/{id:[a-zA-Z0-9]+}", redirect).Methods("GET")
+
+	fmt.Println(dec_to_base62("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 3843))
+
+	http.Handle("/", r)
+	http.ListenAndServe(":"+"8000", nil)
+
 }
